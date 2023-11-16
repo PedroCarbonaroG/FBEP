f1f36b0aeab25ba8b0f8ef6d969956c6fa5a8cec
Pedro Carbonaro <pedrocarbonaro73@gmail.com>
2023-11-15 21:35:54 -0300

    Project Created

---
+++ src/main/java/com/fbep/FBEP/FbepApplication.java
@@ -0,0 +1,13 @@
+package com.fbep.FBEP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FbepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FbepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fbep/FBEP/FbepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fbep.FBEP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FbepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
